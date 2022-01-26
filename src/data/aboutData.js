6cc74d702a9dadbf9432601fd8f0a013b69894e9
bannu0snake <mahaboob.developer@gmail.com>
2022-01-26 09:47:58 +0530

    made initial changes

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,8 @@
 export const aboutData = {
-    title: "Who I am",
-    description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
-    image: 2
-}
+    title: "Who I am ?",
+    description1:
+        "My name's Mahaboob Shaik. I am III Year Under Graduate Student From Indian Institute of Information Technology, Sri City",
+    description2:
+        "I am Full Stack Web Developer and Flutter App Developer. I am Passionate in Development, I am App Dev Lead for GDSC IIITS. I have done couple of Internships in Web Development and a Freelancing Project in Flutter App Development.",
+    image: 2,
+};
