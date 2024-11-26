9be84435d99278157eb511cf8cc0d30f26be4aa5
reshwanth-19535 <reshwanth-19535@reshwanth-19535.csez.zohocorpin.com>
2024-11-26 23:41:03 +0530

    adding experience

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,16 +1,23 @@
 export const experienceData = [
   {
-    id: 2,
+    id: 1,
     company: "Guvi",
     jobtitle: "Question Creator/ Validator (FreeLancing)",
     startYear: "Apr 2023",
     endYear: "May 2023",
   },
   {
-    id: 1,
+    id: 2,
     company: "Virtusa",
     jobtitle: "Full Stack Developer Intern",
     startYear: "Jun 2022",
     endYear: "Apr 2023",
+  },
+  {
+    id: 3,
+    company: "Zoho",
+    jobtitle: "Member of Technicial Staff",
+    startYear: "Oct 2023",
+    endYear: "Present",
   },
 ];
